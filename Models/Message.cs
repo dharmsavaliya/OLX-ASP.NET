725b192287b781d346f7d78c08952f8131122a99
dharmsavaliya <dharmsavaliya5@gmail.com>
2025-04-21 11:07:41 +0530

    ALL DONE

---
+++ Models/Message.cs
@@ -0,0 +1,22 @@
+﻿namespace ASP.NET_OLX.Models
+{
+    public class Message
+    {
+        public int ID { get; set; }
+        public string SenderName { get; set; }      // Changed from SenderId
+        public string ReceiverName { get; set; }    // Changed from ReceiverId
+        public int ProductId { get; set; }
+        public string Content { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+
+
+    public class MessageViewModel
+    {
+        public Product Product { get; set; }
+        public Seller Seller { get; set; }
+        public List<Message> Messages { get; set; }
+    }
+
+}
+
